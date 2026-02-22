4a3a2b8ef515ff1396700a26e1ff6d2e3c1d3660
Rafael de Mello <rafaeldemello2023@gmail.com>
2026-02-22 16:14:00 -0300

    Add Update Componnents clean code

--- react-study/src/App.tsx
+++ react-study/src/App.tsx
@@ -1,14 +1,81 @@
+import { useState } from "react";
 import { Count } from "./components/count";
+import { InputAdd } from "./components/inputAdd";
 import { Lists } from "./components/lists";
+import { TodoItem } from "./components/todoList";
 
 export function App() {
+
+  const [list, setList] = useState([
+      { id: "1", label: "Fazer café", complete: false },
+      { id: "2", label: "Fazer almoço", complete: false },
+      { id: "3", label: "Fazer jantar", complete: false },
+    ]);
+  
+    const handleAdd = (value: string) => {
+       setList([
+                ...list,
+                { id: (list.length + 1).toString(), label: value, complete: false },
+              ]);
+    }
+  
+    const handleRemove = (id: string) => {
+       setList([
+        ...list.filter(item => item.id !== id)
+       ])
+    }
+  
+  const handleComplete = (id: string) => {
+       setList([
+        ...list.map(item => ({
+          ...item,
+          complete: item.id === id ? true : item.complete
+        }))
+       ])
+    }
+
   return (
     <>
       <div style={{ textAlign: "center", marginTop: "20px" }}>
         <Count />
       </div>
-      <div style={{ textAlign: "center", border: "1px solid black", marginTop: "20px", padding: "20px", margin: "20px auto", width: "50%" }}>
-        <Lists />
+      <div
+        style={{
+          textAlign: "center",
+          border: "1px solid black",
+          marginTop: "20px",
+          padding: "20px",
+          margin: "20px auto",
+          width: "50%",
+        }}
+      >
+        <>
+          <div>
+            <InputAdd onAdd={handleAdd} />
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                marginTop: "20px",
+              }}
+            >
+              <Lists>
+                
+                  {list.map((listItem) => (
+                    <TodoItem
+                      key={listItem.id}
+                      id={listItem.id}
+                      label={listItem.label}
+                      complete={listItem.complete}
+                      onComplete={() => handleComplete(listItem.id)}
+                      onRemove={() => handleRemove(listItem.id)}
+                    />
+                  ))}
+                
+              </Lists>
+            </div>
+          </div>
+        </>
       </div>
     </>
   );
--- react-study/src/components/lists.tsx
+++ react-study/src/components/lists.tsx
@@ -1,59 +1,9 @@
-import { useState } from "react";
-import { InputAdd } from "./inputAdd";
-import { TodoItem } from "./todoList";
-
-export function Lists() {
-  const [list, setList] = useState([
-    { id: "1", label: "Fazer café", complete: false },
-    { id: "2", label: "Fazer almoço", complete: false },
-    { id: "3", label: "Fazer jantar", complete: false },
-  ]);
-
-  const handleAdd = (value: string) => {
-     setList([
-              ...list,
-              { id: (list.length + 1).toString(), label: value, complete: false },
-            ]);
-  }
-
-  const handleRemove = (id: string) => {
-     setList([
-      ...list.filter(item => item.id !== id)
-     ])
-  }
-
-const handleComplete = (id: string) => {
-     setList([
-      ...list.map(item => ({
-        ...item,
-        complete: item.id === id ? true : item.complete
-      }))
-     ])
-  }
-  
-
+export const Lists = ({ children }: { children: React.ReactNode }) => { 
   return (
     <>
-      <div>
-        <InputAdd
-          onAdd={handleAdd}
-          
-        />
-        <div style={{display: "flex",justifyContent: "center",marginTop: "20px",}}>
           <ol>
-            {list.map((listItem) => (
-              <TodoItem
-                key={listItem.id}
-                id={listItem.id}
-                label={listItem.label}
-                complete={listItem.complete}
-                onComplete={() => handleComplete(listItem.id)}
-                onRemove={() => handleRemove(listItem.id)}
-              />
-            ))}
+            {children}
           </ol>
-        </div>
-      </div>
     </>
   );
 }
