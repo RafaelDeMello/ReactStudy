d4be65335f16ecde70ce9902606c473c385bf3ab
Rafael de Mello <rafaeldemello2023@gmail.com>
2026-02-24 22:25:20 -0300

    add Create About component

---
+++ react-study/src/pages/About.tsx
@@ -0,0 +1,11 @@
+import { PageLayout } from "../shared/layout/page-layout/PageLayout"
+
+
+
+export const About = () => {
+    return (
+        <PageLayout title="About">
+            <h1>About Page</h1>
+        </PageLayout>
+    )
+}
