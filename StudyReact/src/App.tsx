11ee955b23e60dbbbbb188fc6513d0bb1375eac5
Rafael de Mello <rafaeldemello2023@gmail.com>
2026-05-20 22:54:06 -0300

    Update App.tsx

--- StudyReact/src/App.tsx
+++ StudyReact/src/App.tsx
@@ -1,13 +1,49 @@
+// componente app com contador
+
 import { useState } from "react"
 
+export function App () {
 
-export function App(){
-
-  const [count, setCount] = useState(0)
+const [count, setCount] = useState(0)
 
   return(
     <>
-     <button onClick={() =>{setCount(count + 1)}}>{count}</button>
+     <button onClick={()=> {setCount(count + 1)}}>{count}</button>
     </>
   )
 }
+
+//Funcao JS
+const teste = () => {
+  return 1 + 1
+}
+
+//Funcao JS
+const useTest = () => {
+  return 1 + 1
+}
+
+//REACT HOOK
+const useTeste = () => {
+    const [value] = useState(1 + 1)
+    return value
+}
+//Funcao JS que retorna HTML
+const testes = () => {
+  return (
+    <div>HTML</div>
+  )
+}
+
+//Componente funcional HTMl
+const Test = () => {
+  return (
+    <div>HTML </div>
+  )
+}
+
+
+
+
+
+
