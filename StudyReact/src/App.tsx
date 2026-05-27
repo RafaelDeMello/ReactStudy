3702b210988c326c2d90a1187043ccd94023ecd9
Rafael de Mello <rafaeldemello2023@gmail.com>
2026-05-26 23:23:18 -0300

    Add

--- StudyReact/src/App.tsx
+++ StudyReact/src/App.tsx
@@ -1,76 +1,13 @@
-// componente app com contador
-import React, { useState } from "react"
-import { Exemplos } from "./componentes/Esboco";
-//declarar interface props
-interface ICardProps {
-  tittle: string;
-  children: React.ReactNode;
-}
-
-// Declarar componente car
-const Card = (props: ICardProps) => {
-  return(
-    <div className="flex flex-col p-5 border-2 gap-1">
-      <div>{props.tittle}</div>
-      <div>{props.children}</div>
-      <div>Footer</div>
-    </div>
-  )
-}
 
 
 export function App(){
-
-  const [count, setCount] = useState(0)
-
   return(
     <>
-     <div className="flex flex-col items-center m-1 gap-2">
-      <Card tittle="Titulo 1">
-        Card1
-      </Card>
-      <Card tittle="Titulo 2">
-        Card 2
-      </Card>
-      <Card tittle="Count">
-        <div className="flex bg-black text-white items-center justify-center rounded-2xl">
-          <button onClick={() => {setCount(count + 1)}}>{count}</button>
-        </div>
-      </Card>
-    </div>
-    <Exemplos />
     </>
   )
 }
 
-//Funcao JS
-const teste = () => {
-  return 1 + 1
-}
-//Funcao JS
-const useTeste = () => {
-  return 1 + 1
-}
 
-//REACT HOOK
-const useTestes = () => {
-    const [valor] = useState(1+1)
-    return valor
-}
-
-//Funcao JS que retorna HTML
-const testes = () => {
-  return(
-    <div>Hello</div>
-  )
-}
-
-//Componente funcional HTMl
-const Teste = () => {
-  return(
-    <div>Hello</div>
-  )
-}
 
 
 
--- StudyReact/src/componentes/Esboco.tsx
+++ StudyReact/src/componentes/Esboco.tsx
@@ -1,39 +1,69 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 
-interface ICardProps{
-    tittle: string;
-    children: React.ReactNode;
+interface ICardProps {
+  tittle: string;
+  children: React.ReactNode;
 }
 
-const Card = (props: ICardProps) =>{
-    return(
-        <div className="flex flex-col p-4 gap-3 border-2">
-            <div>{props.tittle}</div>
-            <div>{props.children}</div>
-            <div>Footer</div>
-        </div>
-    )
+const Card = (props: ICardProps) => {
+  return (
+    <div className="flex flex-col p-4 gap-3 border-2">
+      <div>{props.tittle}</div>
+      <div>{props.children}</div>
+      <div>Footer</div>
+    </div>
+  );
+};
+
+export function Exemplos() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="flex flex-col items-center p-2 gap-2.5">
+        <Card tittle="Titulo 1">
+          <div>Card 1</div>
+        </Card>
+        <Card tittle="Titulo 2">
+          <div>Card 2</div>
+        </Card>
+        <Card tittle="Count">
+          <div className="flex items-center justify-center bg-black text-white rounded-3xl">
+            <button
+              onClick={() => {
+                setCount(count + 1);
+              }}
+            >
+              {count}
+            </button>
+          </div>
+        </Card>
+      </div>
+    </>
+  );
 }
 
-export function Exemplos(){
-    
-    const [count, setCount] = useState(0)
+//Funcao JS
+const teste = () => {
+  return 1 + 1;
+};
+//Funcao JS
+const useTeste = () => {
+  return 1 + 1;
+};
 
-    return(
-        <>
-         <div className="flex flex-col items-center p-2 gap-2.5">
-            <Card tittle="Titulo 1"> 
-              <div>Card 1</div>
-            </Card>
-            <Card tittle="Titulo 2"> 
-              <div>Card 2</div>
-            </Card>
-            <Card tittle="Count"> 
-              <div className="flex items-center justify-center bg-black text-white rounded-3xl">
-                 <button onClick={()=>{setCount(count + 1)}}>{count}</button>
-              </div>
-            </Card>
-         </div>
-        </>
-    )
-}
+//REACT HOOK
+const useTestes = () => {
+  const [valor] = useState(1 + 1);
+  return valor;
+};
+
+//Funcao JS que retorna HTML
+const testes = () => {
+  return <div>Hello</div>;
+};
+
+//Componente funcional HTMl
+const Teste = () => {
+  return <div>Hello</div>;
+};
