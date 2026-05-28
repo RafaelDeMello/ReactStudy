ddfa946560fd05ff5502234876fa30bd847e1a9c
Rafael de Mello <rafaeldemello2023@gmail.com>
2026-05-27 22:29:45 -0300

    Update App.tsx

--- StudyReact/src/App.tsx
+++ StudyReact/src/App.tsx
@@ -1,8 +1,20 @@
+import { useState } from "react"
 
 
 export function App(){
+
+  const [count, setCount] = useState(0)
+  const [hide, setHide] = useState(true)
+
   return(
     <>
+    <div>
+      {hide && <p>Hide</p>}
+      {!hide && <p>!Hide</p>}
+      <button onClick={() => {setCount(count + 1)}}>{count}</button>
+      <br />
+      <button onClick={() => {setHide(!hide)}}>Toggle</button>
+    </div>
     </>
   )
 }
