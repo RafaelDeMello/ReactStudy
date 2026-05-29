2264e2670b6fbeb7cf42c0b046b0595a39c77e74
Rafael de Mello <rafaeldemello2023@gmail.com>
2026-05-28 23:34:20 -0300

    Update App.tsx

--- StudyReact/src/App.tsx
+++ StudyReact/src/App.tsx
@@ -2,22 +2,21 @@ import { useState } from "react"
 
 
 export function App(){
-
+    
   const [count, setCount] = useState(0)
   const [hide, setHide] = useState(true)
-
+  
   return(
     <>
-    <div>
-      {hide && <p>Hide</p>}
-      {!hide && <p>!Hide</p>}
-      
-      {hide ? <p>teste1</p> : <p>Teste2</p>}
-
-      <button onClick={() => {setCount(count + 1)}}>{count}</button>
-      <br />
-      <button onClick={() => {setHide(!hide)}}>Toggle</button>
-    </div>
+      <div className="flex flex-col items-center">
+        {hide && <p>Hello</p>}
+        {!hide && <p>Destroyer World</p>}
+        {hide ? <p>Poisitvo</p> : <p>Negativo</p>}
+        <button onClick={() => {setHide(!hide)}}>Alert</button>
+      </div>
+      <div className="flex flex-col items-center">
+        <button onClick={() => {setCount(count + 1)}}>{count}</button>
+      </div>
     </>
   )
 }
