eb09dbdccfda22a5202b2ae6217909d31078f91d
Rafael de Mello <rafaeldemello2023@gmail.com>
2026-05-29 00:26:11 -0300

    Update App.tsx

--- StudyReact/src/App.tsx
+++ StudyReact/src/App.tsx
@@ -1,11 +1,29 @@
+import { useState } from "react"
 
 
 
 export function App(){
     
+  const [list, setList] = useState([
+    'Tarefa 1',
+    'Tarefa 2',
+    'Tarefa 3'
+  ])
   
   return(
     <>
+    <div className="flex flex-col items-center p-5 gap-1">
+      <input className="border" />
+      <button className="border p-1" onClick={() => {}}>Add</button> 
+
+
+      <div>
+        <ol className="list-decimal">
+          {list.map((listItem) =>
+          <li>{listItem}</li>)}
+        </ol>
+      </div>
+    </div>
     
     </>
   )
