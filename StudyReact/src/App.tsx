53c3684f6ed061b4ad693242c9291f837d50159d
Rafael de Mello <rafaeldemello2023@gmail.com>
2026-05-28 23:55:03 -0300

    Update Esboco

--- StudyReact/src/App.tsx
+++ StudyReact/src/App.tsx
@@ -1,22 +1,12 @@
-import { useState } from "react"
+
 
 
 export function App(){
     
-  const [count, setCount] = useState(0)
-  const [hide, setHide] = useState(true)
   
   return(
     <>
-      <div className="flex flex-col items-center">
-        {hide && <p>Hello</p>}
-        {!hide && <p>Destroyer World</p>}
-        {hide ? <p>Poisitvo</p> : <p>Negativo</p>}
-        <button onClick={() => {setHide(!hide)}}>Alert</button>
-      </div>
-      <div className="flex flex-col items-center">
-        <button onClick={() => {setCount(count + 1)}}>{count}</button>
-      </div>
+    
     </>
   )
 }
--- StudyReact/src/componentes/Esboco.tsx
+++ StudyReact/src/componentes/Esboco.tsx
@@ -2,32 +2,34 @@ import React, { useState } from "react";
 
 interface ICardProps {
   tittle: string;
-  children: React.ReactNode;
+  children: React.ReactNode
+  footer: string;
 }
 
 const Card = (props: ICardProps) => {
   return (
-    <div className="flex flex-col p-4 gap-3 border-2">
+    <div className="flex flex-col p-4 gap-3 border-2 items-center">
       <div>{props.tittle}</div>
       <div>{props.children}</div>
-      <div>Footer</div>
+      <div>{props.footer}</div>
     </div>
   );
 };
 
 export function Exemplos() {
   const [count, setCount] = useState(0);
+  const [hide, setHide] = useState(true)
 
   return (
     <>
       <div className="flex flex-col items-center p-2 gap-2.5">
-        <Card tittle="Titulo 1">
+        <Card tittle="Titulo 1" footer="footer">
           <div>Card 1</div>
         </Card>
-        <Card tittle="Titulo 2">
+        <Card tittle="Titulo 2" footer="footer">
           <div>Card 2</div>
         </Card>
-        <Card tittle="Count">
+        <Card tittle="Count" footer="">
           <div className="flex items-center justify-center bg-black text-white rounded-3xl">
             <button
               onClick={() => {
@@ -36,6 +38,14 @@ export function Exemplos() {
             >
               {count}
             </button>
+          </div>
+        </Card>
+        <Card tittle="SetHide or Hide" footer="Done">
+          <div className="flex flex-col items-center">
+            {hide && <p>HELLO WORLD</p>}
+            {!hide && <p>DESTROYER WORLD</p>}
+            {hide ? <p>TRUE</p> : <p>FALSE</p>}
+            <button className="border p-1 rounded-2xl bg-black text-white" onClick={() => {setHide(!hide)}}> Alert </button>
           </div>
         </Card>
       </div>
