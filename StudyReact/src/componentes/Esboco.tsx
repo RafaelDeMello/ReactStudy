bd219b4aab7d83260ad12b60da686a89ce4c6e97
Rafael de Mello <rafaeldemello2023@gmail.com>
2026-06-02 15:07:07 -0300

    Update Esboco.tsx

--- StudyReact/src/componentes/Esboco.tsx
+++ StudyReact/src/componentes/Esboco.tsx
@@ -1,62 +1,71 @@
-import React, { useState } from "react";
+import type React from "react";
+import { useState } from "react";
 
 interface ICardProps {
-  tittle: string;
+  title: string,
   children: React.ReactNode
-  footer: string;
-}
+  footer: string,
+};
 
 const Card = (props: ICardProps) => {
   return (
-    <div className="flex flex-col p-4 gap-3 border-2 items-center">
-      <div>{props.tittle}</div>
+    <div className="border-2 p-5 flex flex-col items-center">
+      <div>{props.title}</div>
       <div>{props.children}</div>
       <div>{props.footer}</div>
     </div>
   );
 };
 
-export function Exemplos() {
-  const [count, setCount] = useState(0)
-  const [hide, setHide] = useState(true)
+const List = () => {
+
   const [list, setList] = useState([
     {id: '1', label: 'Fazer Café'},
     {id: '2', label: 'Fazer Almoço'},
-    {id: '2', label: 'Fazer Janta'},
+    {id: '3', label: 'Fazer Janta'},
   ])
+
+   return (
+      <div className="p-5">
+        <div className="flex gap-2">
+          <input className="border-2"/>
+          <button className="bg-black rounded-3xl text-white px-1">Add Task</button>
+        </div>
+        <ol className="flex flex-col items-center">
+          {list.map((listItem) => 
+          <li key={listItem.id}>{listItem.label}</li>)}
+        </ol>
+      </div>
+   );
+};
+
+export function Exemplos() {
+  
+  const [count, setCount] = useState(0)
+  const [hide, setHide] = useState(true)
+  
 
   return (
     <>
-      <div className="flex  flex-col items-center m-5 gap-3">
-        <Card tittle="Titulo 1" footer="footer">
-          <div>Card 1</div>
+      <div className="flex flex-col items-center m-5 gap-5">
+        <Card title="Title 1" footer="Footer">
+          <div>Card</div>
         </Card>
-        <Card tittle="Count" footer="Footer"> 
-          <div>
-            <button className="border rounded-3xl p-1 bg-black text-white" onClick={() => (setCount(count + 1))}>{count}</button>
-          </div>
+        <Card title="Count" footer="SetCount">
+           <div>
+              <button className="bg-black text-white p-2 rounded-3xl" onClick={() => {(setCount(count + 1))}}>{count}</button>
+           </div>
         </Card>
-        <Card tittle="SetHide or Hide" footer="Done">
-          <div className="flex flex-col items-center">
-            {hide ? <p>Hello World</p> : <p>Bye World</p>}
-          <button className="border rounded-3xl p-1 bg-black text-white" onClick={() => {setHide(!hide)}}>Click</button>
-          </div>
-       </Card>
-        <div className="border-black">
-          <div>
-             <input className="border" /> <button className="border px-1">Add task</button>
-          </div>
-          <div className="border p-3">
-            <ol>
-              {list.map((listItem) =>
-              <li key={listItem.id}>
-                 {listItem.label}
-              </li>)}
-            </ol>
-         </div>
-        </div>
+        <Card title="Hide or SetHide" footer="Hello or Bye World">
+           <div className="p-5 flex flex-col">
+              {hide ? <p>Hello World</p> : <p>Bye World</p>}
+             <button className="bg-black text-white p-1 rounded-3xl" onClick={() => {(setHide(!hide))}}>Click</button>
+           </div>
+        </Card> 
+        <Card title="TodoList" footer="Tarefas">
+          <List />
+        </Card>
       </div>
-
     </>
   );
 }
