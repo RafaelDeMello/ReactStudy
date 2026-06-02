2edd68ac129b73d7d4b4435fd203e63bbfca5a3e
Rafael de Mello <rafaeldemello2023@gmail.com>
2026-06-02 17:37:54 -0300

    Update App.tsx

--- StudyReact/src/App.tsx
+++ StudyReact/src/App.tsx
@@ -1,41 +1,45 @@
-import { useState } from "react"
+import { useState } from "react";
 
-
-
-export function App(){
-    
+export function App() {
+  const [value, setValue] = useState("");
   const [list, setList] = useState([
-     {id: '1', label: 'Fazer Café'},
-     {id: '2', label: 'Fazer Almoço'},
-     {id: '3', label: 'Fazer Janta'}
-  ])
-  
-  
-  return(
+    { id: "1", label: "Fazer Café" },
+    { id: "2", label: "Fazer Almoço" },
+    { id: "3", label: "Fazer Janta" },
+  ]);
+
+  return (
     <>
       <div className="flex flex-col items-center p-1.5">
         <div className="flex items-center gap-1">
-          <input className="border" />
-          <button className="border" onClick={() => {(setList(list))}}>Cadastrar</button>
+          <input
+            className="border"
+            value={value}
+            onChange={(e) => {
+              setValue(e.target.value);
+            }}
+          />
+          <button
+            className="border"
+            onClick={() => {
+              setList([
+                ...list,
+                { id: (list.length + 1).toString(), label: value },
+              ]);
+              setValue("")
+            }}
+          >
+            Cadastrar
+          </button>
         </div>
         <div>
           <ol className="">
-            {list.map((listItem) =>
-            <li key={listItem.id}>
-              {listItem.label}
-            </li>)}
+            {list.map((listItem) => (
+              <li key={listItem.id}>{listItem.label}</li>
+            ))}
           </ol>
         </div>
       </div>
     </>
-  )
+  );
 }
-
-
-
-
-
-
-
-
-
