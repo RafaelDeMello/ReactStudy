fb56207318d41eeaf1c113dc56b14639931760c5
Rafael de Mello <rafaeldemello2023@gmail.com>
2026-06-05 21:57:10 -0300

    Update App.tsx

--- StudyReact/src/App.tsx
+++ StudyReact/src/App.tsx
@@ -1,30 +1,31 @@
 import { useState } from "react";
 
 export function App() {
-  const [value, setValue] = useState("");
+
+  const [value, setValue] = useState('')
   const [list, setList] = useState([
-    { id: "1", label: "Tarefa 1" },
-    { id: "2", label: "Tarefa 2" },
-  ]);
+    {id: '1' , label: 'Tarefa 1'},
+    {id: '2' , label: 'Tarefa 2'},
+  ])
 
   return (
     <>
       <div className="flex flex-col items-center">
         <div className="flex gap-2 mt-3">
           <input className="border-2" 
-           value={value}
-           onChange={(e) => {
-            setValue(e.target.value)
-           }}
+            value={value}
+            onChange={(e) => {
+              setValue(e.target.value)
+            }}
           />
           <button
             className="border rounded-3xl px-3"
             onClick={() => {
               setList([
                 ...list,
-                { id: (list.length + 1).toString(), label: value}
+                { id: (list.length + 1).toString(), label: value},
               ])
-              setValue("")
+              setValue('')
             }}
           >
             Adicionar tarefa
