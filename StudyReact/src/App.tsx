45427af78cd77bd38e87eca6b79ed755875c5c52
Rafael de Mello <rafaeldemello2023@gmail.com>
2026-06-15 22:38:24 -0300

    Código Refatorado

--- StudyReact/src/App.tsx
+++ StudyReact/src/App.tsx
@@ -8,19 +8,20 @@ export function App() {
     { id: "3", label: "Task 3", complete: false },
   ]);
 
+  const handleAdd = (value: string) => {
+    setList([
+       ...list,
+      {id: (list.length + 1).toString(), label: value, complete: false}
+    ])
+  }
+  
   return (
     <>
       <div className="flex flex-col items-center">
         <div className="flex gap-2 mt-3">
           <InputAdd
-             onAdd={(value) => {
-              setList([
-                ...list,
-                {id: (list.length + 1).toString(), label: value, complete: false}
-              ])
-             }}
+            onAdd={handleAdd}
           />
-          
         </div>
         <ol>
           {list.map((listItem) => (
@@ -33,20 +34,21 @@ export function App() {
                   setList([
                     ...list.map((item) => ({
                       ...item,
-                      complete: item.id === listItem.id ? true : item.complete
-                    }))
-                  ])
+                      complete: item.id === listItem.id ? true : item.complete,
+                    })),
+                  ]);
                 }}
               >
                 v
               </button>
-              <button className="border rounded-3xl px-3 m-2"
-                 onClick={() => {
-                   setList([
-                    ...list.filter(item => item.id !== listItem.id)
-                   ])
-                 }}
-              >x</button>
+              <button
+                className="border rounded-3xl px-3 m-2"
+                onClick={() => {
+                  setList([...list.filter((item) => item.id !== listItem.id)]);
+                }}
+              >
+                x
+              </button>
             </li>
           ))}
         </ol>
--- StudyReact/src/componentes/Input.tsx
+++ StudyReact/src/componentes/Input.tsx
@@ -1,36 +1,27 @@
 import { useState } from "react";
 
-
 interface IInputAddProps {
-  onAdd(value): void
+  onAdd(value: string): void,
 }
 
 export const InputAdd = (props: IInputAddProps) => {
+  const [value, setValue] = useState("");
 
-    const [value, setValue] = useState('')
+  const getValue = (e: any) => {
+    setValue(e.target.value);
+  };
 
-    return(
-        <div>
-            <input
-            className="border"
-            value={value}
-            onChange={(e) => {
-              setValue(e.target.value)
-            }}
-          />
-          <button
-            className="border rounded-3xl px-3 m-2"
-            onClick={() => {
-              if(!value){
-                alert('Insira uma Task!')
-                return
-              }
-              props.onAdd(value);
-              setValue('')
-            }}
-          >
-            Add Task
-          </button>
-        </div>
-    )
-}
+  const handleAdd = () => {
+      props.onAdd(value),
+      setValue('')
+  };
+
+  return (
+    <div>
+      <input className="border" value={value} onChange={getValue} />
+      <button className="border rounded-3xl px-3 m-2" onClick={handleAdd}>
+        Add Task
+      </button>
+    </div>
+  );
+};
