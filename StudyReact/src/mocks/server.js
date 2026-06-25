3abfe11bdf524fe043b9b9a76a13ae0af7ba5fa5
Rafael de Mello <rafaeldemello2023@gmail.com>
2026-06-24 22:37:14 -0300

    Update server.js

--- StudyReact/src/mocks/server.js
+++ StudyReact/src/mocks/server.js
@@ -7,9 +7,18 @@ createServer({
 
     routes() {
 
-        this.namespace = "api"
-
+        this.namespace = "api";
+        
         this.get('/todos', (schema) => {
            return schema.all('todos');
         })
+        
+        this.post('/todos', (schema, request) => {
+             const attrs = JSON.parse(request.requestBody)
+
+             const todo = schema.create('todos', attrs);
+
+             return todo
+        })
+    }
 })
