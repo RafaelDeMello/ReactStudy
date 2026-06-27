1540f93a79b2cc598cd85dedd8e2623bcf4310eb
Rafael de Mello <rafaeldemello2023@gmail.com>
2026-06-27 11:41:23 -0300

    todoApi Check

--- StudyReact/src/shared/api/todoApi.ts
+++ StudyReact/src/shared/api/todoApi.ts
@@ -8,31 +8,29 @@ interface ITodo {
   complete: boolean;
 }
 
-interface ItodoWithoutId {
+interface ITodoWithOut {
   label: string;
   complete: boolean;
 }
 
 export const TodoApi = {
-  async getAll(){
-    const response = await axiosInstance.get('/api/todos')
+  async getAll() {
+    const response = await axiosInstance.get("/api/todos");
 
-    return response.data.todos as ITodo[]
+    return response.data.todos as ITodo[];
   },
 
-  async create(data: ItodoWithoutId){
-     const response = await axiosInstance.post('/api/todos', data)
-     
-     return response.data.todo as ITodo
+  async create(data: ITodoWithOut) {
+    const response = await axiosInstance.post("/api/todos", data);
+
+    return response.data.todos as ITodo;
   },
 
-  async updateById(id: string, data: Partial<ItodoWithoutId>) {
-    await axiosInstance.put(`/api/todos/${id}`, data);
-
-    return;
+  async updateById(id: string, data: Partial<ITodoWithOut>) {
+    return await axiosInstance.put(`/api/todos/${id}`, data);
   },
 
   async deleteById(id: string) {
-    await axiosInstance.delete(`/api/todos/${id}`);
+    return await axiosInstance.delete(`/api/todos/${id}`);
   },
 };
