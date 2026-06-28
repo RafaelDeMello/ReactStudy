64c0e2d43a74bab20a7179123a40e2610209f9d7
Rafael de Mello <rafaeldemello2023@gmail.com>
2026-06-28 11:46:36 -0300

    Update server.ts

--- StudyReact/src/mocks/server.ts
+++ StudyReact/src/mocks/server.ts
@@ -1,6 +1,5 @@
 import { createServer, Model, Factory } from "miragejs";
 
-
 createServer({
   models: {
     todos: Model,
@@ -20,30 +19,32 @@ createServer({
       return schema.all("todos");
     });
 
-    (this.post("/todos", (schema, request) => {
+    this.post("/todos", (schema, request) => {
       const attrs = JSON.parse(request.requestBody);
 
       const todo = schema.create("todos", attrs);
 
       return todo;
-    }),
-      this.put("/todos/:id", (schema, request) => {
-        const id = request.params.id;
+    });
 
-        const newAttrs = JSON.parse(request.requestBody);
+    this.put("/todos/:id", (schema, request) => {
+      const id = request.params.id;
 
-        const todo = schema.find("todos", id);
-        todo?.update(newAttrs);
+      const newAttrs = JSON.parse(request.requestBody);
 
-        return {};
-      }),
-      this.delete("/todos/:id", (schema, request) => {
-        const id = request.params.id;
+      const todo = schema.find("todos", id);
+      todo?.update(newAttrs);
 
-        const todo = schema.find("todos", id);
-        todo?.destroy();
+      return {};
+    });
 
-        return {};
-      }));
+    this.delete("/todos/:id", (schema, request) => {
+      const id = request.params.id;
+
+      const todo = schema.find("todos", id);
+      todo?.destroy();
+
+      return {};
+    });
   },
 });
