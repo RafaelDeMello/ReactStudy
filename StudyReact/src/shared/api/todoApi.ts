e10d6329e3e2fd3479605a4596a565dd7e854507
Rafael de Mello <rafaeldemello2023@gmail.com>
2026-06-28 14:02:00 -0300

    Update todoApi.ts

--- StudyReact/src/shared/api/todoApi.ts
+++ StudyReact/src/shared/api/todoApi.ts
@@ -19,18 +19,16 @@ export const TodoApi = {
 
     return response.data.todos as ITodo[];
   },
-
   async create(data: ITodoWithOut) {
     const response = await axiosInstance.post("/api/todos", data);
 
     return response.data.todos as ITodo;
   },
-
   async updateById(id: string, data: Partial<ITodoWithOut>) {
     return await axiosInstance.put(`/api/todos/${id}`, data);
   },
 
   async deleteById(id: string) {
-    return await axiosInstance.delete(`/api/todos/${id}`)
+    return await axiosInstance.delete(`/api/todos/${id}`);
   },
 };
